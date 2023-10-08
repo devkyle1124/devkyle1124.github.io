67e6033ddb1abcd089585f216af8d6c87df8c2fe
devkyle1124 <145661673+devkyle1124@users.noreply.github.com>
2023-10-08 11:50:49 +0800

    Add files via upload

---
+++ colormadness/js/script.js
@@ -0,0 +1,38 @@
+var docRef = db.collection("OCTOBER").doc("Me88 Color Madness");
+var iframe = document.getElementById("my-iframe");
+var loadingIndicator = document.getElementById("loading-indicator");
+
+// Show the loading indicator immediately after a short delay
+setTimeout(function () {
+    loadingIndicator.style.display = "flex";
+}, 100);
+
+function hideLoadingIndicator() {
+    loadingIndicator.style.display = "none";
+}
+
+function setIframeSource(val, game) {
+    if (val) {
+        iframe.src = game;
+    } else {
+        iframe.src = "./dist/index.html";
+    }
+}
+
+iframe.onload = function () {
+    hideLoadingIndicator(); // Hide loading indicator when iframe is loaded
+};
+
+docRef.onSnapshot(function (doc) {
+    if (doc.exists) {
+        var data = doc.data();
+        var val = data.value;
+        var game = data.myGame;
+
+        setIframeSource(val, game);
+    } else {
+        // Handle when the document doesn't exist
+    }
+}, function (error) {
+    // Handle errors
+});
